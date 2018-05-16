f469e064542af562c479c71a078a3593d5018680
Magda Gutowska <m.gutowska0909@gmail.com>
2018-05-16 23:56:58 +0200

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+create: function() {
+	var self = this;
+	this.$element = $('<button>');
+	this.$element.text(this.text);
+	this.$element.click(function() {
+		alert(self.text);
+		this.$element.appendTo($('body'));
+	});
+	
+}
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
